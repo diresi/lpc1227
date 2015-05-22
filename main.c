a24dc72d6fa99bf1ae16109ce971b8152aa6d4c3
Christoph Rissner <resi@rissner.net>
2015-05-22 19:16:49 +0200

    removed trailing whitespace

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*! 
+/*!
     @file     main.c
     @author   K. Townsend (microBuilder.eu)
 
@@ -44,7 +44,7 @@
 #endif
 
 /**************************************************************************/
-/*! 
+/*!
     Main program entry point.  After reset, normal code execution will
     begin here.
 */
@@ -73,8 +73,8 @@ int main(void)
     }
 
     // Poll for CLI input if CFG_INTERFACE is enabled in projectconfig.h
-    #ifdef CFG_INTERFACE 
-      cmdPoll(); 
+    #ifdef CFG_INTERFACE
+      cmdPoll();
     #endif
   }
 
