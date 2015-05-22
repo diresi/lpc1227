b32eeaa96252780ef9d6e9ee55bf266eac79bcd6
Christoph Rissner <resi@rissner.net>
2015-05-22 19:17:49 +0200

    LPC1227 interrupt vectors

--- lpc1xxx/LPC11xx_handlers.c
+++ lpc1xxx/LPC11xx_handlers.c
@@ -28,11 +28,11 @@
  *
  */
 
-// The GCC compiler defines the current architecture derived from the -mcpu argument. 
+// The GCC compiler defines the current architecture derived from the -mcpu argument.
 // When target cpu is the cortex-m0, it automatically defines __ARM_ARCH_6M__
-// Some versions define __ARM_ARCH_6SM__ instead 
-#if !defined(__ARM_ARCH_6M__) && !defined(__ARM_ARCH_6SM__) 
-  #error "The target ARM cpu must be Cortex-M0 compatible (-mcpu=cortex-m0)" 
+// Some versions define __ARM_ARCH_6SM__ instead
+#if !defined(__ARM_ARCH_6M__) && !defined(__ARM_ARCH_6SM__)
+  #error "The target ARM cpu must be Cortex-M0 compatible (-mcpu=cortex-m0)"
 #endif
 
 // Declare a weak alias macro as described in the GCC manual[1][2]
@@ -49,26 +49,24 @@ void irq_undefined() {
   while(1);
 }
 
-void CAN_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
-void SSP1_IRQHandler(void)      WEAK_ALIAS(irq_undefined);
 void I2C_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
 void TIMER16_0_IRQHandler(void) WEAK_ALIAS(irq_undefined);
 void TIMER16_1_IRQHandler(void) WEAK_ALIAS(irq_undefined);
 void TIMER32_0_IRQHandler(void) WEAK_ALIAS(irq_undefined);
 void TIMER32_1_IRQHandler(void) WEAK_ALIAS(irq_undefined);
-void SSP0_IRQHandler(void)      WEAK_ALIAS(irq_undefined);
-void UART_IRQHandler(void)      WEAK_ALIAS(irq_undefined);
-void USB_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
-void USB_FIQHandler(void)       WEAK_ALIAS(irq_undefined);
+void SSP_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
+void UART0_IRQHandler(void)     WEAK_ALIAS(irq_undefined);
+void UART1_IRQHandler(void)     WEAK_ALIAS(irq_undefined);
+void COMP_IRQHandler(void)      WEAK_ALIAS(irq_undefined);
 void ADC_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
 void WDT_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
 void BOD_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
-void FMC_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
-void PIOINT3_IRQHandler(void)   WEAK_ALIAS(irq_undefined);
 void PIOINT2_IRQHandler(void)   WEAK_ALIAS(irq_undefined);
 void PIOINT1_IRQHandler(void)   WEAK_ALIAS(irq_undefined);
 void PIOINT0_IRQHandler(void)   WEAK_ALIAS(irq_undefined);
 void WAKEUP_IRQHandler(void)    WEAK_ALIAS(irq_undefined);
+void DMA_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
+void RTC_IRQHandler(void)       WEAK_ALIAS(irq_undefined);
 
 /*****************************************************************************
  * Forward undefined fault handlers to an infinite loop function. The Handlers
@@ -99,66 +97,66 @@ void SysTick_Handler(void)      WEAK_ALIAS(fault_undefined);
 extern void* stack_entry;
 extern void boot_entry(void);
 
-// Defined irq vectors using simple c code following the description in a white 
+// Defined irq vectors using simple c code following the description in a white
 // paper from ARM[3] and code example from Simonsson Fun Technologies[4].
 // These vectors are placed at the memory location defined in the linker script
 const void *vectors[] SECTION(".irq_vectors") =
 {
   // Stack and program reset entry point
-  &stack_entry,          // The initial stack pointer
-  boot_entry,            // The reset handler
+  &stack_entry,          // The initial stack pointer 0x00
+  boot_entry,            // The reset handler         0x04
 
   // Various fault handlers
-  NMI_Handler,           // The NMI handler
-  HardFault_Handler,     // The hard fault handler
-  MemManage_Handler,     // MemManage_Handler
-  BusFault_Handler,      // BusFault_Handler
-  UsageFault_Handler,    // UsageFault_Handler
-  0,                     // Reserved
-  0,                     // Reserved
-  0,                     // Reserved
-  0,                     // Reserved
-  SVCall_Handler,        // SVCall handler
-  DebugMon_Handler,      // DebugMon_Handler
-  0,                     // Reserved
-  PendSV_Handler,        // The PendSV handler
-  SysTick_Handler,       // The SysTick handler
-  
+  NMI_Handler,           // The NMI handler           0x08
+  HardFault_Handler,     // The hard fault handler    0x0c
+  MemManage_Handler,     // MemManage_Handler         0x10
+  BusFault_Handler,      // BusFault_Handler          0x14
+  UsageFault_Handler,    // UsageFault_Handler        0x18
+  0,                     // Reserved                  0x1c
+  0,                     // Reserved                  0x20
+  0,                     // Reserved                  0x24
+  0,                     // Reserved                  0x28
+  SVCall_Handler,        // SVCall handler            0x2c
+  DebugMon_Handler,      // DebugMon_Handler          0x30
+  0,                     // Reserved                  0x34
+  PendSV_Handler,        // The PendSV handler        0x38
+  SysTick_Handler,       // The SysTick handler       0x3c
+
   // Wakeup I/O pins handlers
-  WAKEUP_IRQHandler,     // PIO0_0  Wakeup
+  WAKEUP_IRQHandler,     // PIO0_0  Wakeup            0x40
   WAKEUP_IRQHandler,     // PIO0_1  Wakeup
   WAKEUP_IRQHandler,     // PIO0_2  Wakeup
   WAKEUP_IRQHandler,     // PIO0_3  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_4  Wakeup
+  WAKEUP_IRQHandler,     // PIO0_4  Wakeup            0x50
   WAKEUP_IRQHandler,     // PIO0_5  Wakeup
   WAKEUP_IRQHandler,     // PIO0_6  Wakeup
   WAKEUP_IRQHandler,     // PIO0_7  Wakeup
-  WAKEUP_IRQHandler,     // PIO0_8  Wakeup
+  WAKEUP_IRQHandler,     // PIO0_8  Wakeup            0x60
   WAKEUP_IRQHandler,     // PIO0_9  Wakeup
   WAKEUP_IRQHandler,     // PIO0_10 Wakeup
   WAKEUP_IRQHandler,     // PIO0_11 Wakeup
-  WAKEUP_IRQHandler,     // PIO1_0  Wakeup
-  
+
   // Specific peripheral irq handlers
-  CAN_IRQHandler,        // CAN
-  SSP1_IRQHandler,       // SSP1
-  I2C_IRQHandler,        // I2C0
+  I2C_IRQHandler,        // SI (state change)         0x70
   TIMER16_0_IRQHandler,  // CT16B0 (16-bit Timer 0)
   TIMER16_1_IRQHandler,  // CT16B1 (16-bit Timer 1)
   TIMER32_0_IRQHandler,  // CT32B0 (32-bit Timer 0)
-  TIMER32_1_IRQHandler,  // CT32B1 (32-bit Timer 1)
-  SSP0_IRQHandler,       // SSP0
-  UART_IRQHandler,       // UART0
-  USB_IRQHandler,        // USB IRQ
-  USB_FIQHandler,        // USB FIQ
+  TIMER32_1_IRQHandler,  // CT32B1 (32-bit Timer 1)   0x80
+  SSP_IRQHandler,        // SSP
+  UART0_IRQHandler,      // UART0
+  UART1_IRQHandler,      // UART1
+  COMP_IRQHandler,       // Comparator                0x90
   ADC_IRQHandler,        // ADC (A/D Converter)
   WDT_IRQHandler,        // WDT (Watchdog Timer)
   BOD_IRQHandler,        // BOD (Brownout Detect)
-  FMC_IRQHandler,        // Flash (IP2111 Flash Memory Controller)
-  PIOINT3_IRQHandler,    // PIO INT3
-  PIOINT2_IRQHandler,    // PIO INT2
-  PIOINT1_IRQHandler,    // PIO INT1
+  0,                     // Reserved                  0xA0
   PIOINT0_IRQHandler,    // PIO INT0
+  PIOINT1_IRQHandler,    // PIO INT1
+  PIOINT2_IRQHandler,    // PIO INT2
+  0,                     // Reserved                  0xB0
+  DMA_IRQHandler,        // DMA
+  RTC_IRQHandler,        // RTC
+  0,                     // Reserved
 };
 
 /******************************************************************************
